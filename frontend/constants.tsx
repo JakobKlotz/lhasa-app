1c35d2d4a3ccc9b27f5c846c80b6f897cdb242a2
Jakob Klotz <jakob.klotz@mci.edu>
2025-04-29 13:17:14 +0200

    refactor: use `process` to load the env variable

--- frontend/constants.tsx
+++ frontend/constants.tsx
@@ -1,4 +1,1 @@
-// 'NEXT_PUBLIC' prefix is important to be able to access it
-import { env } from 'next-runtime-env';
-
-export const BACKEND_API_BASE_URL = env("NEXT_PUBLIC_BACKEND_API_BASE_URL")
+export const BACKEND_API_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL
